546e86ae025055321abfb6050118b85b60ad8fae
ian <jesperiansantos.2021@gmail.com>
2021-11-04 07:50:49 -0300

    enviando arquivos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aprendendo React</h1>
+      <pre>A coruja é o símbolo da sabedoria por ser um animal noturno e mascote da deusa Atena.</pre>
+      <img src="https://www.iguiecologia.com/wp-content/uploads/2021/02/Coruja-buraqueira7-2.jpg" alt="coruja" />
+    </div>
+  );
+}
+
+export default App;
